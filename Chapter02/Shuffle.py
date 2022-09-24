6a35e059985c5d92aff224f19f32762f3b07cf5e
Sourcery AI <>
2022-09-24 05:45:07 +0000

    'Refactored by Sourcery'

--- Chapter02/Shuffle.py
+++ Chapter02/Shuffle.py
@@ -1,10 +1,8 @@
 import os
 import random
-#initiate a list called emails_list
-emails_list = []
 Directory = '/home/azureuser/spam_filter/enron1/emails/'
 Dir_list = os.listdir(Directory)
 for file in Dir_list:
-f = open(Directory + file, 'r')
-emails_list.append(f.read())
-f.close()
+    pass
+with open(Directory + file, 'r') as f:
+    emails_list = [f.read()]
--- Chapter02/SpamDetection_NLTK.py
+++ Chapter02/SpamDetection_NLTK.py
@@ -12,24 +12,27 @@ def Process(data):
 def Features_Extraction(text, setting):
   if setting=='bow':
    # Bow means bag-of-words
-    return {word: count for word, count in Counter(Process(text)).items() if not word in stop}
+    return {
+        word: count
+        for word, count in Counter(Process(text)).items() if word not in stop
+    }
   else:
-    return {word: True for word in Process(text) if not word in stop}
+    return {word: True for word in Process(text) if word not in stop}
     
 features = [(Features_Extraction(email, 'bow'), label) for (email, label)
 in emails]
 
-def training_Model (Features, samples):
+def training_Model(Features, samples):
   Size = int(len(Features) * samples)
   training , testing = Features[:Size], Features[Size:]
-  print ('Training = ' + str(len(training)) + ' emails')
-  print ('Testing = ' + str(len(testing)) + ' emails')
+  print(f'Training = {len(training)} emails')
+  print(f'Testing = {len(testing)} emails')
   
 classifier = NaiveBayesClassifier.train(training)
 
 def evaluate(training, tesing, classifier):
-  print ('Training Accuracy is ' + str(classify.accuracy(classifier,train_set)))
-  print ('Testing Accuracy i ' + str(classify.accuracy(classifier,test_set)))
+  print(f'Training Accuracy is {str(classify.accuracy(classifier, train_set))}')
+  print(f'Testing Accuracy i {str(classify.accuracy(classifier, test_set))}')
 
 
 
